121cb83a1cde94e79a531ff90770c80e6c75df4d
sankari <wzr980910@163.com>
2024-01-15 09:35:06 +0800

    第一次提交

---
+++ src/main/java/com/platform/OnlineBookstorePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookstorePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookstorePlatformApplication.class, args);
+    }
+
+}
